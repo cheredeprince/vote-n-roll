069efcf32a003102c32902dae01407784fc67f14
La Bécasse <becasse@cheredeprince.net>
2016-12-05 00:36:06 +0100

    wrap-word -> wrap-letter

--- public/javascripts/display.js
+++ public/javascripts/display.js
@@ -628,8 +628,8 @@ var display = (function(){
     text.each(function() {
 
       var text = d3.select(this),
-          words = text.text().split(/\s+/).reverse(),
-          word,
+          letters = text.text().split("").reverse(),
+          letter,
           line = [],
           lineNumber = 0,
           lineHeight = 1.1, // ems
@@ -637,15 +637,15 @@ var display = (function(){
           dy = parseFloat(text.attr("dy")),
           tspan = text.text(null).append("tspan").attr("x", 0).attr("y", y).attr("dy", dy + "em");
 
-      while (word = words.pop()) {
-        line.push(word);
-        tspan.text(line.join(" "));
+      while (letter = letters.pop()) {
+        line.push(letter);
+        tspan.text(line.join(""));
 	
         if (tspan.node().getComputedTextLength() > width) {
           line.pop();
-          tspan.text(line.join(" "));
-          line = [word];
-          tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(word);
+          tspan.text(line.join(""));
+          line = [letter];
+          tspan = text.append("tspan").attr("x", 0).attr("y", y).attr("dy", ++lineNumber * lineHeight + dy + "em").text(letter);
         }
       }
 
