1fd8c0f7858e720a6ec5a14def34bca355fc048d
La Bécasse <becasse@cheredeprince.net>
2017-09-20 19:01:25 +0200

    correction data

--- app.js
+++ app.js
@@ -42,8 +42,6 @@ _.forEach(config.elections,function(election,id){
 
       k++;
       res[id][modeVote] = ballots;
-//      if(k==length)
-//	resultsBoard.init(_.cloneDeep(config.elections),_.cloneDeep(config.scrutins),res);
     });
 })
 })
